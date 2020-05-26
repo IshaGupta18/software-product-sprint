9a27e657f0d7262d3fbcbcaaab82e498a3f5f647
IshaGupta18 <ishagupta1828@gmail.com>
2020-05-26 11:54:32 +0530

    feat: remove unnecessary attendees from the events attendee list and take up only the non-empty events

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,47 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    // Remove all attendees from an event who are not in the meeting request
+    // This way, if an event has no attendees important to us, that interval of time is freed up for scheduling the meeting
+
+    Iterator eventsIterator = events.iterator();
+    ArrayList<TimeRange> blockedTimeRanges = new ArrayList<TimeRange>();
+
+    while (eventsIterator.hasNext()){
+      
+      Event event = (Event) eventsIterator.next();
+      Set<String> eventAttendees = event.getAttendees();
+      Iterator allAttendeesIterator = request.getAttendees().iterator();
+      Set<String> newEventAttendees = new HashSet<String>();
+      
+      while (allAttendeesIterator.hasNext()){
+        String person = (String) allAttendeesIterator.next();
+        
+        // If an event contains an attendee from meeting request, add it to the new attendee list for that event 
+
+        if (eventAttendees.contains(person)){
+          newEventAttendees.add(person);
+        }
+      }
+
+      // If the attendee list of the Event is not empty ie atleast one person from the meeting list attends this event, then block this time range as its unavailable to hold a meeting
+
+      if (!newEventAttendees.isEmpty()){
+        blockedTimeRanges.add(event.getWhen());
+      }
+    }
+
+    ArrayList<TimeRange> availableTimeRanges = new ArrayList<TimeRange>();
+
+    return availableTimeRanges;
   }
 }
