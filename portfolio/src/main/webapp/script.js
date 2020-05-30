8983136cd875a21c9df964a7c9c74067618f45d9
IshaGupta18 <ishagupta1828@gmail.com>
2020-05-31 00:07:10 +0530

    refactor: separate function for creating name and mood section of a comment

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -45,19 +45,24 @@ function getBadgeColour(mood){
   }
   
 }
-function createCommentCard(content,name,mood=null){
-  let outerDiv = createDivElement("card");
-  let contentDiv = createDivElement("card-body");
-  contentDiv.appendChild(createTextHTMLElement("P",content,"card-text"));
-  outerDiv.appendChild(contentDiv);
+
+function createNameMoodSectionComment(name,mood){
   let nameMoodDiv = createDivElement("card-header");
   let nameParagraph = createTextHTMLElement("P",name);
   if (mood){
     nameParagraph.appendChild(createTextHTMLElement("span",mood,"badge badge-pill badge-"+getBadgeColour(mood)));
   }
   nameMoodDiv.appendChild(nameParagraph);
-  outerDiv.appendChild(nameMoodDiv);
-  console.log(outerDiv);
+  return nameMoodDiv;
+}
+function createCommentCard(content,name=null,mood=null){
+  let outerDiv = createDivElement("card");
+  let contentDiv = createDivElement("card-body");
+  contentDiv.appendChild(createTextHTMLElement("P",content,"card-text"));
+  outerDiv.appendChild(contentDiv);
+  if (name){
+    outerDiv.appendChild(createNameMoodSectionComment(name,mood));
+  }
   document.getElementById("comments-container").appendChild(outerDiv);
 }
 function addRandomGreeting() {
@@ -93,7 +98,7 @@ function fetchComments(){
   fetch('/data').then(response => response.json()).then((comments)=>{
     comments.forEach((comment) => {
       comment = getCommentObject(comment);
-      if (comment!=null) {
+      if (comment!=null && comment["content"]) {
         createCommentCard(comment["content"],comment["name"],comment["mood"]);
       }
     });
