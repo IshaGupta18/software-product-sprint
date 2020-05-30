b2a086c8b9f08a7b641b7b5f222bf7ce564f1f82
IshaGupta18 <ishagupta1828@gmail.com>
2020-05-31 00:07:47 +0530

    refactor: remove unused greetings function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -65,17 +65,6 @@ function createCommentCard(content,name=null,mood=null){
   }
   document.getElementById("comments-container").appendChild(outerDiv);
 }
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
 function createTextHTMLElement(elementName,content,className=null){
   let element = document.createElement(elementName);
   if (className){
